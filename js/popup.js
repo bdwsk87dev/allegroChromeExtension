8a3dd5e03bc1f016ff05067398b529269afc7520
alex <bdwsk87dev@gmail.com>
2023-01-23 13:23:55 +0200

    Fixes

--- js/popup.js
+++ js/popup.js
@@ -5,11 +5,21 @@ var popupDownloader = {
 			chrome.extension.sendRequest({action: "start", minPrice: minPrice}, function () {});
 			//window.close();
 		})
+        $("#generateCSV").click(()=>{
+            popupDownloader.generateCSV();
+        });
     },
     onMessage: function (request, sender, callback){
-        if (request.action == "logging"){
+        if (request.action === "logging"){
             $('#logger').html($('#logger').html() + "<br>" + request.message);
         }
+    },
+
+    generateCSV: function (){
+        $('#logger').html('!!!');
+        fetch('https://allegro.pl/kategoria/iii-rp-1990-5-zl-okolicznosciowe-249502').then(data=>{
+            data.text().then(data => console.log(data));
+        })
     }
 }
 $(function () {
