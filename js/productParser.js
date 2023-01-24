c3db1740427bfeeb4a992e13feb26907aa304058
alex <bdwsk87dev@gmail.com>
2023-01-24 10:10:55 +0200

    continue hard working

--- js/productParser.js
+++ js/productParser.js
@@ -1,10 +1,7 @@
 var productParser = {
     init:function (){
-
         let productName = document.getElementsByTagName('h4')[0].innerHTML;
-
-
-
+        let description = document.querySelector('div[data-box-name="Description card"]');
     }
 }
 
