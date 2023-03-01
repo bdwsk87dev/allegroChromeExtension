ec55e5f00744c704509018cbef31a902754ff542
alex <bdwsk87dev@gmail.com>
2023-03-01 13:41:24 +0200

    items find fix

--- js/pageParser.js
+++ js/pageParser.js
@@ -3,7 +3,7 @@ let coreParser = {
 
     init: function () {
         /** Get localStorageData minPrice and maxPrice **/
-        chrome.storage.local.get(null, function(fetchedData) {
+        chrome.storage.local.get(null, function (fetchedData) {
             coreParser.minPrice = fetchedData.minPrice;
             coreParser.maxPrice = fetchedData.maxPrice;
             /** Call parsing method */
@@ -13,8 +13,12 @@ let coreParser = {
 
     /** Find all products link on the page **/
     parseData: function () {
-       // let items = document.querySelectorAll('div.opbox-listing article div div div:first-child a:first-child');
+        // let items = document.querySelectorAll('div.opbox-listing article div div div:first-child a:first-child');
         let items = document.querySelectorAll('div.opbox-listing h2:nth-child(2) a');
+        if (items.length === 0) {
+            items = document.querySelectorAll('div[data-box-name="items container"] h2 a');
+        }
+
         items.forEach(item => {
             coreParser.products.push({'url': item.getAttribute('href')});
         });
